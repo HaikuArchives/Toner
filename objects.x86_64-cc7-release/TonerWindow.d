2b433a7295f4bb16c85eeba9180251f23d19bba7
trungnt2910 <trungnt282910@gmail.com>
2020-01-12 18:09:13 +0700

    Cleaned up

--- objects.x86_64-cc7-release/TonerWindow.d
+++
@@ -1,4 +0,0 @@
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS FOLLOW ---
-objects.x86_64-cc7-release/TonerWindow.o : TonerView.h TonerWindow.h
-
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS PRECEDE ---
